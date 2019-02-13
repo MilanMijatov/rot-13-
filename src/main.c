fff41793390eee85da71478681275d8c806026d6
Milan Mijatov <milanmijatov0@gmail.com>
2019-02-13 18:57:24 +1100

    More boilerplate

--- src/main.c
+++ src/main.c
@@ -2,5 +2,12 @@
 
 int main(int argc, char *argv[])
 {
-    
+    if(argc = 1)
+    {
+        printf("Expected 1 or more arguments\nProper usage %s \"text here\"\nAlternatively %s text here"
+                    , argv[0], argv[0]);
+        return 0;
+    }
+
+    return 0;
 }
